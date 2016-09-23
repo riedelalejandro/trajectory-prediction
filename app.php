fdfff32dcbd323dac39e9b1d92a4125fd6c2cb63
riedelalejandro@gmail.com <riedelalejandro@gmail.com>
2016-09-23 00:11:32 -0300

    unused files

--- app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include './vendor/autoload.php';
-
-$builder = \TrajectoryPrediction\Predictor::create();
-$builder->withDefaultMonthly(1000);
-$builder->withDistanceHistory(\Carbon\Carbon::create(2016, 9, 1), 15000);
-$builder->andWithDistanceHistory(\Carbon\Carbon::create(2016, 9, 22), 16000);
-dd($builder->getTrajectoryPeriods(), $builder->predict(16200));
